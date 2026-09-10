a5524f9b58baad8ab0a576908ab367d23926458c
ehinmitan-tosin <149206721+ehinmitan-tosin@users.noreply.github.com>
2026-09-10 11:34:30 -0700

    Modify AWS region and configure S3 bucket website
    Updated AWS region and added S3 bucket website configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
-resource "aws_s3_bucket" "resume_website" {
-  bucket = "tosin-resume"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
+
+  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
@@ -29,4 +15,16 @@ provider "aws" {
 
 resource "aws_s3_bucket" "resume_website" {
   bucket = "tosin-resume"
+}
+
+resource "aws_s3_bucket_website_configuration" "resume_website" {
+  bucket = aws_s3_bucket.resume_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
 }
